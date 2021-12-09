dfa946f980efb8804445be8defb7086e78f43018
guillaumerumeau <VrRcB-Z9wqwh>
2021-12-09 15:27:47 +0100

    Ajout de Primes et State migrés de scala 2 en scala 3 (strict) Ajout d'un exemple de migration 2 vers 3 pour l'ancien usage des implicits

---
+++ src/main/scala/Primes.scala
@@ -0,0 +1,45 @@
+package org.rg.su3
+
+import scala.util.Try
+
+object Primes :
+
+  private lazy val primes : LazyList[BigInt] =
+    inline def from(begin: BigInt, interval: Int): LazyList[BigInt] =
+      begin #:: from(begin + interval, interval)
+
+    BigInt(2) #:: from(BigInt(3), 2).filter {
+      n => primes.takeWhile(j => j * j <= n).forall(m => m == n || (n % m != 0))
+    }
+  end primes
+
+  /**
+   * Flux des nombres premiers positifs
+   */
+  def apply() : LazyList[BigInt] = primes
+
+  /**
+   * Récupérer le nième nombre premier positif dans l'ordre croissant
+   */
+  def apply(n : Int) : BigInt = primes(n)
+
+  /**
+   * Vérifie la primalité d'un nombre
+   * Recherche exhaustive
+   */
+  def isPrime(i : BigInt) : Boolean =
+    val ai = i.abs
+    if ai <2 then false
+    else primes.takeWhile ( _ <= ai ).last == ai
+  end isPrime
+
+  private inline val certainly : Int = 48
+
+  /**
+   * Vérifie la primalité d'un nombre
+   * Recherche probabiliste
+   */
+  def isPPrime(i : BigInt) : Boolean =
+    i.isProbablePrime(certainly)
+
+end Primes
---
+++ src/main/scala/State.scala
@@ -0,0 +1,31 @@
+package org.rg.su3
+
+trait State[S, A] :
+  val run: S => (S, A)
+  def apply(s: S): (S, A) = run(s)
+  def eval(s: S): A = run(s)._2
+  def exec(s: S): S = run(s)._1
+
+  def map[B](f: A => B): State[S, B] =
+    State { (s: S) =>
+      val (s1, a) = run(s)
+      (s1, f(a))
+    }
+
+  def flatMap[B](f: A => State[S, B]): State[S, B] =
+    State { (s: S) =>
+      val (s1, a) = run(s)
+      f(a)(s1)
+    }
+
+object State :
+  def apply[S, A](f: S => (S, A)): State[S, A] = new State[S, A] {
+    final val run = f
+  }
+
+  def init[S, A](a: A) = State { (s : S) => (s, a) }
+  def update[S, A](f: S => S): State[S, Unit] = State { (s: S) => (f(s), ()) }
+  def get[S] : State[S, S] = State { s => (s, s) }
+  def gets[S, A](f: S => A): State[S, A] = State { (s: S) => (s, f(s)) }
+
+  def put[S](s : S) : State[S, Unit] = State { _ => (s, ()) }
