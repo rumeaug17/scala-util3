f8d4a616025dda9654d1ea716cac456b9a106bf9
Rumeau Guillaume <rumeaug17@gmail.com>
2021-12-13 16:11:19 +0100

    Create PositiveIntegers.scala
    Ajout PositiveIntegers

---
+++ src/main/scala/PositiveIntegers.scala
@@ -0,0 +1,21 @@
+package org.rg.su3
+
+object PositiveIntegers :
+  
+  opaque type Positive = Int
+
+  object Positive :
+    def apply(x : Int) : Positive =
+      require( x >= 0)
+      x
+
+    def safe(x : Int) : Option[Positive] =
+      if x >= 0 then Some(x) else None
+
+  extension (x : Positive)
+    def +(y : Positive) : Positive = x + y
+    def *(y : Positive) : Positive = x * y
+    def -(y : Positive) : Positive = 
+      require(x >= y)    
+      x - y
+end PositiveIntegers
