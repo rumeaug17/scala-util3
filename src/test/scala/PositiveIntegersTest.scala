b3d9e0a12ad0ebbf519549c155f2aa3c0e7d1005
guillaumerumeau <VrRcB-Z9wqwh>
2021-12-21 21:03:19 +0100

    Ajout de tests pour PositiveIntegers

---
+++ src/test/scala/PositiveIntegersTest.scala
@@ -0,0 +1,26 @@
+package org.rg.su3
+
+import scala.collection.immutable
+import org.scalatest.funsuite.AnyFunSuite
+
+import scala.language.implicitConversions
+import PositiveIntegers.*
+import PositiveIntegers.PositiveOrdering.mkOrderingOps
+
+class PositiveIntegersTest extends AnyFunSuite:
+
+  test("1 is a positive integer") {
+    val p = Positive(1)
+    assert(p == 1)
+  }
+
+  test("1 + 1 = 2") {
+    val p = Positive(1)
+    assert(p + p == Positive(2))
+  }
+
+  test("1 < 2") {
+    assert(Positive(1) < 2)
+  }
+
+// add error test with negative int
